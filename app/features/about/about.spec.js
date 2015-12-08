e0aa2d360c5f3d39618a5919fb1901f636e2ab92
James Wright <james.wright@capita.co.uk>
2015-12-08 15:53:19 +0000

    About spec green

--- app/features/about/about.spec.js
+++ app/features/about/about.spec.js
@@ -1,24 +1,20 @@
 (function() {
   "use strict";
 
-describe("Controller: aboutCtrl", function () {
+  describe("Controller: aboutCtrl", function () {
 
-  // load the controller"s module
-  beforeEach(module("sampleApp.about"));
+    // load the controller"s module
+    beforeEach(module("sampleApp.about"));
 
-  var aboutCtrl,
-    scope;
+    var aboutCtrl;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    aboutCtrl = $controller("aboutCtrl", {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller) {
+      aboutCtrl = $controller("aboutCtrl");
+    }));
+
+    it("should attach a list of awesomeThings to the scope", function () {
+      expect(aboutCtrl.awesomeThings.length).toBe(3);
     });
-  }));
-
-  it("should attach a list of awesomeThings to the scope", function () {
-    expect(scope.awesomeThings.length).toBe(3);
   });
-});
 })();
